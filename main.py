12724586063437491ef4297c6ac910ced5b835cb
Kyuwoong Lee <kwooler@gmail.com>
2022-10-10 20:34:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-name = input().split()
-score = []
+import numpy as np
+import matplotlib.pyplot as plt
+
+Math = [100, 90]
+English = [90, 80]
+Physics = [80, 80]
+Computer = [80, 90]
 		
 # #####################
 # Make your coe
 # #####################
 
-# Use this print 
-# print (f'{name[i]} {total} {avg:.2f}')
+fig.savefig('midtermq5.png')
